3d64219781dfa2c1e886d8dd9b7bd2c56ee8a72e
saltchicken <john.eicher89@gmail.com>
2024-04-13 11:00:44 -0700

    Full duplex

--- server.py
+++ server.py
@@ -16,20 +16,11 @@ async def handle_client(websocket, path):
     except ConnectionClosed:
         logger.debug(f"{path} disconnected")
 
-# async def send_message(message):
-#     # Iterate over connected clients and send the message to each one
-#     for client in connected_clients:
-#         await client.send(message)
-
-# async def send_input():
-#     while True:
-#         message = input("Enter message to send to clients: ")
-#         for ws in connected_clients:
-#             await ws.send(message)
-
 async def get_user_input():
     loop = asyncio.get_event_loop()
     user_input = await loop.run_in_executor(None, input, "Enter something: ")
+    for ws in connected_clients:
+            await ws.send(user_input)
     return user_input
 
 async def main():
@@ -42,14 +33,5 @@ async def main():
                 print("You entered:", user_input)
         except KeyboardInterrupt:
             logger.debug("Keyboard interrupt. Main function closing")
-        # await asyncio.Future()  # run forever
-    # server = await websockets.serve(echo, "localhost", 8765)
-    # asyncio.create_task(send_input())
-    # await server.wait_closed()
-        # while True:
-        #     message = input("Enter message to send (or 'quit' to exit): ")
-        #     if message == 'quit':
-        #         break
-        #     await send_message(message)
 
 asyncio.run(main())
