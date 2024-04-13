2a993f52bbaef6abe183241c1bb5e6dc6d39ed2d
saltchicken <john.eicher89@gmail.com>
2024-04-13 11:06:39 -0700

    user_input coroutine created and removed from main loop

--- server.py
+++ server.py
@@ -16,7 +16,13 @@ async def handle_client(websocket, path):
     except ConnectionClosed:
         logger.debug(f"{path} disconnected")
 
-async def get_user_input():
+async def input_routine():
+    # TODO: How can I properly break this loop?
+    while True:
+        user_input = await get_user_input_and_send()
+        print("You entered:", user_input)
+
+async def get_user_input_and_send():
     loop = asyncio.get_event_loop()
     user_input = await loop.run_in_executor(None, input, "Enter something: ")
     for ws in connected_clients:
@@ -25,13 +31,8 @@ async def get_user_input():
 
 async def main():
     async with websockets.serve(handle_client, "localhost", 8765):
-        # asyncio.create_task(send_input())
-        # print('go forever')
-        try:
-            while True:
-                user_input = await get_user_input()
-                print("You entered:", user_input)
-        except KeyboardInterrupt:
-            logger.debug("Keyboard interrupt. Main function closing")
+        asyncio.create_task(input_routine())
+        await asyncio.Future()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
