b8ae1b203c8675b38bd177df7ad0c6a53cbc7652
saltchicken <john.eicher89@gmail.com>
2024-04-13 22:33:16 -0700

    Refactor experimental

---
+++ experimental/classes.py
@@ -0,0 +1,33 @@
+from better_websocket.classes import Server
+import multiprocessing, queue, asyncio
+
+from loguru import logger
+
+class BetterServerController():
+    def __init__(self):
+        self.q = multiprocessing.Queue()
+        self.process = multiprocessing.Process(target=self.start_server, args=(self.q,))
+        self.process.start()
+
+    def start_server(self, queue):
+        server = BetterServer(queue)
+        server.run()
+
+class BetterServer(Server):
+    def __init__(self, queue):
+        super().__init__()
+        self.queue = queue
+    
+    # def process_input(self, input):
+    #     # return super().process_input(input)
+    #     logger.debug(f"Process this: {input}")
+
+    async def send(self):
+        try:
+            message = self.queue.get_nowait()
+            logger.debug(message)
+            return message
+        except queue.Empty:
+            # logger.debug("Queue was empty")
+            await asyncio.sleep(0.3)
+            # return "Nope"
--- server_test_process.py
+++ server_test_process.py
@@ -1,38 +1,5 @@
-from better_websocket.classes import Server
-import multiprocessing, queue, asyncio
 
-from loguru import logger
-
-class BetterServerController():
-    def __init__(self):
-        self.q = multiprocessing.Queue()
-        self.process = multiprocessing.Process(target=self.start_server, args=(self.q,))
-        self.process.start()
-
-    def start_server(self, queue):
-        server = BetterServer(queue)
-        server.run()
-
-class BetterServer(Server):
-    def __init__(self, queue):
-        super().__init__()
-        self.queue = queue
-    
-    # def process_input(self, input):
-    #     # return super().process_input(input)
-    #     logger.debug(f"Process this: {input}")
-
-    async def send(self):
-        try:
-            message = self.queue.get_nowait()
-            logger.debug(message)
-            return message
-        except queue.Empty:
-            # logger.debug("Queue was empty")
-            await asyncio.sleep(0.3)
-            # return "Nope"
-
-
+from experimental.classes import BetterServerController
 
 if __name__ == "__main__":
     server_controller = BetterServerController()
